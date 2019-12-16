91780bf8c7f2cc8071db17509a4e9e68d0319e4e
siansiong97 <56067813+siansiong97@users.noreply.github.com>
2019-12-16 12:25:15 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,7 +40,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
         AppRoutingModule,
         IonicStorageModule.forRoot(),
         LaravelPassportModule.forRoot(
-            {apiRoot: 'http://localhost:8000', clientId: 2, clientSecret: 'm6fenXUbXpUodDuz48OPKi4djKkSYn1XgmdHQ8RK'}
+            {apiRoot: 'http://localhost:8000', clientId: 2, clientSecret: 'iTkjVHqP0xuY47Rp2UyqGYpRNAxc9oCmi9YgxWt1'}
         ),
         PerfectScrollbarModule,
         IpohDrumPageModule
