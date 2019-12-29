cc1d41bd23e5dd01912e7ead3c0d4b20faf189f5
henrylow97 <henry_lcz97@hotmail.com>
2019-12-29 10:38:16 +0800

    -Development commit.
    -Finished designing on Sales Video page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import {IpohDrumPageModule} from './ipoh-drum/ipoh-drum.module';
         AppRoutingModule,
         IonicStorageModule.forRoot(),
         LaravelPassportModule.forRoot(
-            {apiRoot: 'http://localhost:8000', clientId: 2, clientSecret: '9kaBaOD3yWgkPMCSK56Wc09UBfzTNyt4vLFa6Mri'}
+            {apiRoot: 'http://localhost:8000', clientId: 2, clientSecret: 'W96z4CaQVwGVgXnhuZPnPBk0pzcR4W7Q5GXyMNt5'}
         ),
         IpohDrumPageModule
     ],
